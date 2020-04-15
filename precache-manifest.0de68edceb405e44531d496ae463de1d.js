a921b2fc0bd1e6269badc4ae7b0ae11c2ccc11cc
Dina Mansour <dinamansour.dev2000@gmail.com>
2020-04-16 01:28:33 +0200

    Updates

---
+++ precache-manifest.0de68edceb405e44531d496ae463de1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bb94e4e35d6fd02bff5c74931d6455",
+    "url": "/TVSeries/index.html"
+  },
+  {
+    "revision": "9154bf7340a76a67e9fe",
+    "url": "/TVSeries/static/css/main.40b1d25c.chunk.css"
+  },
+  {
+    "revision": "e9259968c0819b9db4d6",
+    "url": "/TVSeries/static/js/2.ddac3c6f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TVSeries/static/js/2.ddac3c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9154bf7340a76a67e9fe",
+    "url": "/TVSeries/static/js/main.aca07373.chunk.js"
+  },
+  {
+    "revision": "4cca3c5ceb7c1335cea3",
+    "url": "/TVSeries/static/js/runtime-main.a8119b3e.js"
+  },
+  {
+    "revision": "b0a23662fc29583bdc0361913e8f75d6",
+    "url": "/TVSeries/static/media/loader.b0a23662.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TVSeries/static/media/logo.5d5d9eef.svg"
+  }
+]);
